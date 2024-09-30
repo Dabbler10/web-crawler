d070b5f10e4f0dba285792534487620be5e4fdb1
Dabbler10 <dabbler503@gmail.com>
2024-09-30 13:46:10 +0500

    add crawler

--- main.py
+++ main.py
@@ -1,54 +1,116 @@
 import requests
 from bs4 import BeautifulSoup
-import csv
-
-def scrape_page(soup, quotes):
-    quote_elements = soup.find_all('div', class_='quote')
-
-    for quote_element in quote_elements:
-        text = quote_element.find('span', class_='text').text
-        author = quote_element.find('small', class_='author').text
-        tag_elements = quote_element.find('div', class_='tags').find_all('a', class_='tag')
-
-        tags = []
-        for tag_element in tag_elements:
-            tags.append(tag_element.text)
-
-        quotes.append(
-            {
-                'text': text,
-                'author': author,
-                'tags': ', '.join(tags)
-            }
-        )
-
-url = 'https://quotes.toscrape.com'
-
-#заголовки для имитации юзера
-headers = {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36'
-}
-
-page = requests.get(url, headers=headers)
-soup = BeautifulSoup(page.text, 'html.parser')
+from urllib.parse import urljoin, urlparse
+import os
+import hashlib
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
-quotes = []
-scrape_page(soup, quotes)
-next_li_element = soup.find('li', class_='next')
+# Папка для сохранения скачанных страниц
+save_dir = "downloaded_pages"
+# Файл для хранения посещённых URL
+saved_files = "saved_urls.txt"
+max_threads = 8
 
-while next_li_element is not None:
-    next_page_relative_url = next_li_element.find('a', href=True)['href']
-    page = requests.get(url + next_page_relative_url, headers=headers)
-    soup = BeautifulSoup(page.text, 'html.parser')
-    scrape_page(soup, quotes)
-    next_li_element = soup.find('li', class_='next')
+# Начальный URL
+start_url = "https://quotes.toscrape.com/"
+# Список допустимых доменов
+allowed_domains = {"quotes.toscrape.com"}
 
-csv_file = open('quotes.csv', 'w', encoding='utf-8', newline='')
-writer = csv.writer(csv_file)
-writer.writerow(['Text', 'Author', 'Tags'])
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
 
-for quote in quotes:
-    writer.writerow(quote.values())
+visited_urls = set()
+saved_urls = set()
 
-csv_file.close()
+
+def load_saved_urls():
+    if os.path.exists(saved_files):
+        with open(saved_files, 'r', encoding='utf-8') as f:
+            return set(line.strip() for line in f)
+    return set()
+
+def add_saved_url(url):
+    with open(saved_files, 'a', encoding='utf-8') as f:
+        f.write(url + "\n")
+
+
+def fetch(url):
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.text
+    except requests.RequestException as e:
+        print(f"Ошибка при запросе к {url}: {e}")
+        return None
+
+
+def is_valid_url(url):
+    parsed = urlparse(url)
+    return bool(parsed.netloc) and bool(parsed.scheme)
+
+
+def is_allowed_domain(url):
+    parsed_url = urlparse(url)
+    domain = parsed_url.netloc
+    if domain.startswith("www."):
+        domain = domain[4:]
+    return domain in allowed_domains
+
+
+def save_page(content, url):
+    file_name = hashlib.md5(url.encode()).hexdigest() + ".html"
+    file_path = os.path.join(save_dir, file_name)
+
+    with open(file_path, 'w', encoding='utf-8') as f:
+        f.write(content)
+    print(f"Страница сохранена: {file_path}")
+
+
+def crawl(url, depth=1):
+    if url in visited_urls or depth <= 0:
+        return
+
+    print(f"Посещаем: {url}\r")
+    visited_urls.add(url)
+
+    html = fetch(url)
+    if html is None:
+        return
+
+    if url not in saved_urls :
+        save_page(html, url)
+        add_saved_url(url)
+
+    soup = BeautifulSoup(html, 'html.parser')
+
+    tasks = []
+    for a_tag in soup.find_all("a", href=True):
+        href = a_tag.get("href")
+        # Приводим относительные URL к полным
+        full_url = urljoin(url, href)
+
+        if is_valid_url(full_url) and full_url not in visited_urls and is_allowed_domain(full_url):
+            tasks.append((full_url, depth - 1))
+
+    return tasks
+
+
+def start_crawl(start_url, depth=2):
+    global visited_urls
+    global saved_urls
+    saved_urls = load_saved_urls()
+
+    with ThreadPoolExecutor(max_workers=max_threads) as executor:
+        futures = [executor.submit(crawl, start_url, depth)]
+
+        while futures:
+            for future in as_completed(futures):
+                tasks = future.result()
+                if tasks:
+                    for url, new_depth in tasks:
+                        futures.append(executor.submit(crawl, url, new_depth))
+            futures = [f for f in futures if not f.done()]
+
+
+start_crawl(start_url, depth=2)
